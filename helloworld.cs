3b2148a287c1dd96f423fa6914cc20aa3ff49d63
Dylan Rhymaun <rhymaundylan@gmail.com>
2024-12-17 20:36:04 -0500

    Consolidated subdirectories into main, rebuilt and renamed stuff

---
+++ helloworld.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningcsharp
+{
+    internal class helloworld
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
